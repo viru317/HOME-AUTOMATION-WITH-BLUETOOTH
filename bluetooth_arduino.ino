28b22fc13a181da932a0b2af1d8bc8cfbacf5f05
M.Virendra Sai <virendrasai153@gmail.com>
2025-06-25 19:36:15 +0530

    Add files via upload

---
+++ bluetooth_arduino.ino
@@ -0,0 +1,27 @@
+String input; 
+void setup() {
+pinMode(7, OUTPUT);  
+Serial.begin(9600);
+} 
+void loop() {
+  while (Serial.available())
+  { 
+  delay(10); 
+  char c = Serial.read(); 
+  input += c; 
+  }  
+  if (input.length() > 0)
+  {
+    Serial.println(input); 
+       if(input == "on")
+       {
+         digitalWrite(7, HIGH);
+        }  
+  if(input == "off")
+  {
+   digitalWrite(7, LOW);
+    } 
+input="";
+}
+}
+
